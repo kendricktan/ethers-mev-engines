d2976a9501239ad7f2a0b531c27aa58784596f55
Kendrick Tan <kendricktan0814@gmail.com>
2021-03-29 18:31:29 +1000

    fixed abi

--- src/abi/MEVBriber.ts
+++ src/abi/MEVBriber.ts
@@ -1,9 +1,120 @@
 export const abi = [
   {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
     inputs: [
-      { internalType: "address", name: "_target", type: "address" },
-      { internalType: "bytes", name: "_payload", type: "bytes" },
-      { internalType: "bytes32", name: "_resultMatch", type: "bytes32" },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "briber",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "miner",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "Bribed",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "PERMIT_TYPEHASH",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "WETH",
+    outputs: [
+      {
+        internalType: "contract IWETH",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "deadline",
+        type: "uint256",
+      },
+      {
+        internalType: "uint8",
+        name: "v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "s",
+        type: "bytes32",
+      },
+    ],
+    name: "briberPermitted",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_target",
+        type: "address",
+      },
+      {
+        internalType: "bytes",
+        name: "_payload",
+        type: "bytes",
+      },
+      {
+        internalType: "bytes32",
+        name: "_resultMatch",
+        type: "bytes32",
+      },
     ],
     name: "check32BytesAndSend",
     outputs: [],
@@ -12,9 +123,21 @@ export const abi = [
   },
   {
     inputs: [
-      { internalType: "address[]", name: "_targets", type: "address[]" },
-      { internalType: "bytes[]", name: "_payloads", type: "bytes[]" },
-      { internalType: "bytes32[]", name: "_resultMatches", type: "bytes32[]" },
+      {
+        internalType: "address[]",
+        name: "_targets",
+        type: "address[]",
+      },
+      {
+        internalType: "bytes[]",
+        name: "_payloads",
+        type: "bytes[]",
+      },
+      {
+        internalType: "bytes32[]",
+        name: "_resultMatches",
+        type: "bytes32[]",
+      },
     ],
     name: "check32BytesAndSendMulti",
     outputs: [],
@@ -23,10 +146,56 @@ export const abi = [
   },
   {
     inputs: [
-      { internalType: "uint256", name: "_bribeAmount", type: "uint256" },
-      { internalType: "address[]", name: "_targets", type: "address[]" },
-      { internalType: "bytes[]", name: "_payloads", type: "bytes[]" },
-      { internalType: "bytes32[]", name: "_resultMatches", type: "bytes32[]" },
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_value",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_deadline",
+        type: "uint256",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+      {
+        internalType: "address[]",
+        name: "_targets",
+        type: "address[]",
+      },
+      {
+        internalType: "bytes[]",
+        name: "_payloads",
+        type: "bytes[]",
+      },
+      {
+        internalType: "bytes32[]",
+        name: "_resultMatches",
+        type: "bytes32[]",
+      },
     ],
     name: "check32BytesAndSendMultiWETH",
     outputs: [],
@@ -35,10 +204,56 @@ export const abi = [
   },
   {
     inputs: [
-      { internalType: "uint256", name: "_bribeAmount", type: "uint256" },
-      { internalType: "address", name: "_target", type: "address" },
-      { internalType: "bytes", name: "_payload", type: "bytes" },
-      { internalType: "bytes32", name: "_resultMatch", type: "bytes32" },
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_value",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_deadline",
+        type: "uint256",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+      {
+        internalType: "address",
+        name: "_target",
+        type: "address",
+      },
+      {
+        internalType: "bytes",
+        name: "_payload",
+        type: "bytes",
+      },
+      {
+        internalType: "bytes32",
+        name: "_resultMatch",
+        type: "bytes32",
+      },
     ],
     name: "check32BytesAndSendWETH",
     outputs: [],
@@ -47,9 +262,21 @@ export const abi = [
   },
   {
     inputs: [
-      { internalType: "address", name: "_target", type: "address" },
-      { internalType: "bytes", name: "_payload", type: "bytes" },
-      { internalType: "bytes", name: "_resultMatch", type: "bytes" },
+      {
+        internalType: "address",
+        name: "_target",
+        type: "address",
+      },
+      {
+        internalType: "bytes",
+        name: "_payload",
+        type: "bytes",
+      },
+      {
+        internalType: "bytes",
+        name: "_resultMatch",
+        type: "bytes",
+      },
     ],
     name: "checkBytesAndSend",
     outputs: [],
@@ -58,9 +285,21 @@ export const abi = [
   },
   {
     inputs: [
-      { internalType: "address[]", name: "_targets", type: "address[]" },
-      { internalType: "bytes[]", name: "_payloads", type: "bytes[]" },
-      { internalType: "bytes[]", name: "_resultMatches", type: "bytes[]" },
+      {
+        internalType: "address[]",
+        name: "_targets",
+        type: "address[]",
+      },
+      {
+        internalType: "bytes[]",
+        name: "_payloads",
+        type: "bytes[]",
+      },
+      {
+        internalType: "bytes[]",
+        name: "_resultMatches",
+        type: "bytes[]",
+      },
     ],
     name: "checkBytesAndSendMulti",
     outputs: [],
@@ -69,10 +308,56 @@ export const abi = [
   },
   {
     inputs: [
-      { internalType: "uint256", name: "_bribeAmount", type: "uint256" },
-      { internalType: "address[]", name: "_targets", type: "address[]" },
-      { internalType: "bytes[]", name: "_payloads", type: "bytes[]" },
-      { internalType: "bytes[]", name: "_resultMatches", type: "bytes[]" },
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_value",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_deadline",
+        type: "uint256",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+      {
+        internalType: "address[]",
+        name: "_targets",
+        type: "address[]",
+      },
+      {
+        internalType: "bytes[]",
+        name: "_payloads",
+        type: "bytes[]",
+      },
+      {
+        internalType: "bytes[]",
+        name: "_resultMatches",
+        type: "bytes[]",
+      },
     ],
     name: "checkBytesAndSendMultiWETH",
     outputs: [],
@@ -81,10 +366,56 @@ export const abi = [
   },
   {
     inputs: [
-      { internalType: "uint256", name: "_bribeAmount", type: "uint256" },
-      { internalType: "address", name: "_target", type: "address" },
-      { internalType: "bytes", name: "_payload", type: "bytes" },
-      { internalType: "bytes", name: "_resultMatch", type: "bytes" },
+      {
+        internalType: "address",
+        name: "_owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_spender",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_value",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "_deadline",
+        type: "uint256",
+      },
+      {
+        internalType: "uint8",
+        name: "_v",
+        type: "uint8",
+      },
+      {
+        internalType: "bytes32",
+        name: "_r",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "_s",
+        type: "bytes32",
+      },
+      {
+        internalType: "address",
+        name: "_target",
+        type: "address",
+      },
+      {
+        internalType: "bytes",
+        name: "_payload",
+        type: "bytes",
+      },
+      {
+        internalType: "bytes",
+        name: "_resultMatch",
+        type: "bytes",
+      },
     ],
     name: "checkBytesAndSendWETH",
     outputs: [],
@@ -92,11 +423,26 @@ export const abi = [
     type: "function",
   },
   {
-    inputs: [],
-    name: "weth",
-    outputs: [{ internalType: "contract IWETH", name: "", type: "address" }],
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "nonces",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
     stateMutability: "view",
     type: "function",
   },
-  { stateMutability: "payable", type: "receive" },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
 ];
